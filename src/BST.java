27c3a9d3e0e8703a693786610a0bdfc6f3573e9d
Merey <misenbai@gmail.com>
2023-05-21 16:22:42 +0600

    3

--- src/BST.java
+++ src/BST.java
@@ -16,8 +16,25 @@ public class BST<K extends Comparable<K>, V> {
         return size;
     }
     public void put(K key, V val){
-
+        root = put(root, key, val);
     }
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            size++;
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val;
+        }
+
+        return node;
+    }
+
     public V get(K key){
         return null;
     }
