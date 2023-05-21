c89e6a5ff10dc46cc1d2c7561c03d51f806d2509
Merey <misenbai@gmail.com>
2023-05-21 12:40:45 +0600

    2

--- src/BST.java
+++ src/BST.java
@@ -1,14 +1,19 @@
 public class BST<K extends Comparable<K>, V> {
     private Node root;
+    private int size = 0;
     public class Node {
         private K key;
         private V val;
         private Node left, right;
 
+
         public Node(K key, V value) {
             this.key = key;
             this.val = val;
         }
+    }
+    public int getSize() {
+        return size;
     }
     public void put(K key, V val){
 
